8e28cb6f6c22725bb51ab7839f0abb3a837de280
MasterSix <zanohelp132@gmail.com>
2026-08-11 15:54:42 -0300

    feat: Bump Kaixa version to 0.2.0-dev

--- src/kaixa.cpp
+++ src/kaixa.cpp
@@ -1,7 +1,9 @@
 #include <kaixa/kaixa.hpp>
 
 namespace kaixa {
+    inline constexpr std::string_view KAIXA_VERSION = "0.2.0-dev";
+
     std::string_view version() noexcept {
-        return "0.1.0";
+        return KAIXA_VERSION;
     }
 }
--- tests/smoke.cpp
+++ tests/smoke.cpp
@@ -7,7 +7,7 @@
 KAIXA_TEST(library_reports_its_version) {
     context.check_equal(
         kaixa::version(),
-        std::string_view("0.1.0"),
+        std::string_view("0.2.0-dev"),
         "library version"
     );
 }
